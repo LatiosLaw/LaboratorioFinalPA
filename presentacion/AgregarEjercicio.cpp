0271a9aa73dbbf0bae4c3728d98781af49c59cb1
MrEasted <115025660+MrEasted@users.noreply.github.com>
2024-06-27 00:56:38 -0300

    Update AgregarEjercicio.cpp
    para que funcione con el codigo de agrEJ que un no se quien lo hizo.

--- presentacion/AgregarEjercicio.cpp
+++ presentacion/AgregarEjercicio.cpp
@@ -58,13 +58,42 @@ void AgregarEjercicio::agregarEjercicio()
 
         cout << "Selecciona el numero de la leccion la cual quieres agregar el ejercicio.." << endl;
         cin >> num;
-        Leccion *l = c->buscarLeccion(num); // obtener la leccion
+        //Leccion *l = c->buscarLeccion(num); //instanciarlo si se precisa para el if de abajo, y comparar esa l con el nullptr si no funciona
 
-        if (l == nullptr) // control que no este vacio.
+        if (c->buscarLeccion(num) == nullptr) // control que no exista la leccion
         {
             cout << "El numero de la leccion ingresada no existe..." << endl;
             return;
         }
 
+        //DATOS DEL EJ
+        cout << "Ingrese el nombre del ejercicio.." << endl;
+        cin >> nomE;
+        cout << "Ingrese la descripcion del ejercicio.." << endl;
+        cin >> descE;
+
+        cout << "Que tipo de ejercicio quieres agregar? /n 1- Completar Palabra  /n 2- Traduccion " << endl;
+        cin >> num;
+        if (num == 1)
+        { // Completar Palabra
+            cout << "Ingrese la frase a completar del ejercicio.." << endl;
+            cin >> fraseA;
+            cout << "Ingrese la frase correcta del ejercicio.." << endl;
+            cin >> fraseB;
+        }
+        else if (num == 2)
+        { // Traduccion
+            cout << "Ingrese la frase a traducir del ejercicio.." << endl;
+            cin >> fraseA;
+            cout << "Ingrese la traduccion correcta del ejercicio.." << endl;
+            cin >> fraseB;
+        }
+        else
+        {
+            cout << "Opcion invalida..." << endl;
+            return;
+        }
+        c->buscarLeccion(num)->agregarEjercicio(nomE,descE,fraseA, string fraseB,num)
+        
     }
 }
