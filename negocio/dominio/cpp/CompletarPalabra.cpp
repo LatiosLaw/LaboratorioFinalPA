6fd0687a0b099ce4f57f39f88d677f5fa973314d
LatiosLaw <77687435+LatiosLaw@users.noreply.github.com>
2024-06-29 22:53:33 -0300

    Algunos endl y detalles

--- negocio/dominio/cpp/CompletarPalabra.cpp
+++ negocio/dominio/cpp/CompletarPalabra.cpp
@@ -25,10 +25,10 @@ return this->fraseCorrecta;
 
 void CompletarPalabra::mostrarEjercicio(){
 cout << "Ejercicio : " << this->getnombreEjercicio() << endl;
-cout << "Complete las palabras faltantes en la siguiente frase." << endl;
+cout << "Complete las palabras faltantes en la siguiente frase de la siguiente manera 'ejemplo, ejemplo' " << endl;
 cout << "Los espacios faltantes seran marcados con guiones." << endl;
 cout << "Frase a completar : " << endl;
-cout << this->getFrase();
+cout << this->getFrase() << endl;
 }
 
 bool CompletarPalabra::enviarSolucion(string solu){
